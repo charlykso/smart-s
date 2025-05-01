ce74f8260b369f0faae7b02321bcd7cabb0d990c
Nweke Chinonso Christian <nwekechristian890@gmail.com>
2025-05-01 06:58:46 -0700

    created the paymentProfile

--- api/server.js
+++ api/server.js
@@ -3,12 +3,10 @@ const  connectDB = require('./db/connection')
 const groupSchoolRoute = require('./route/groupSchoolRoute')
 const schoolRoute = require('./route/schoolRoute')
 const AddressRoute = require('./route/AddressRoute')
-<<<<<<< HEAD
 const ClassArmRoute = require('./route/ClassArmRoute')
-=======
 const userRoute = require('./route/userRoute');
 const profileRoute = require('./route/profileRoute');
->>>>>>> f2b9ca1a33c9c03b8fdcced0dd153f7c2cbf4b5b
+const paymentProfileRoute = require('./route/paymentProfileRoute');
 
 const app = express();
 
@@ -19,12 +17,10 @@ app.use(express.json()); // Middleware to parse JSON requests
 app.use('/api/v1/groupSchool', groupSchoolRoute)
 app.use('/api/v1/school', schoolRoute)
 app.use('/api/v1/Address', AddressRoute)
-<<<<<<< HEAD
 app.use('/api/v1/ClassArm', ClassArmRoute)
-=======
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/profile', profileRoute)
->>>>>>> f2b9ca1a33c9c03b8fdcced0dd153f7c2cbf4b5b
+app.use('/api/v1/paymentProfile', paymentProfileRoute)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
