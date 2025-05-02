00a76dbe76775b123170c9b0e9c450926cbd1e86
Charles Madu <charlesmadu001@gmail.com>
2025-05-02 12:33:34 +0100

    correct mistake

--- api/route/schoolRoute.js
+++ api/route/schoolRoute.js
@@ -4,9 +4,10 @@ const router = express.Router()
 
 router.get('/all',  SchoolController.getSchools)
 router.get('/:id', SchoolController.getSchool)
-router.create('/create', SchoolController.createSchool)
+router.post('/create', SchoolController.createSchool)
 router.put('/update', SchoolController.updateSchool)
 router.delete('/delete', SchoolController.deleteSchool)
 router.get('/by-address/:addressId', SchoolController.getSchoolByAddress)
+router.get('/by-groupSchool/:groupSchoolId', SchoolController.getSchoolByGroupSchool)
 
 module.exports = router
