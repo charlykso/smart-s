ec7b7075cb228cb500ea6fce704e892087e4054c
charlykso <charlykso141@gmail.com>
2025-05-05 15:46:43 +0100

    added school to classarm model

--- api/model/ClassArm.js
+++ api/model/ClassArm.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const ClassArmSchema = new mongoose.Schema({
+    school: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'School',
+        required: true,
+    },
     name: {
         type: String,
         required: true,
     },
     totalNumberOfStudents: {
         type: Number,
-        require: true,
     }
 }, { timestamps: true });
 
