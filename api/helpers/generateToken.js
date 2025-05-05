f114fbdd91ae7a5f1962933f6681a7b5efa8ddfc
obilalumark <lumarkobila@icloud.com>
2025-05-05 16:15:37 +0100

    changes made

--- api/helpers/generateToken.js
+++ api/helpers/generateToken.js
@@ -1,37 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const getAuthToken = (User) => {
-    if (!user || !user._id || !user.roles) {
-        throw new Error("Invalid user data");
-    }
-
-    return jwt.sign(
-        {
-            id: user._id,
-            roles: [user.roles],
-            iat: Math.floor(Date.now() / 1000), 
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '1h',
-        }
-    );
-};
-
-const getRefreshToken = (user) => {
-    return jwt.sign(
-        {
-            id: user._id,
-            roles: user.role,
-            iat: Math.floor(Date.now() / 1000),
-        },
-        process.env.JWT_REFRESH_SECRET,
-        {
-            expiresIn: '5d',
-        }
-    );
-};
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
