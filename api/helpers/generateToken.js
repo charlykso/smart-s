cdce5b6dc6ceade43e4afd020c59530015d623ab
charlykso <charlykso141@gmail.com>
2025-05-05 14:20:26 +0100

    some changes in generateToken function

--- api/helpers/generateToken.js
+++ api/helpers/generateToken.js
@@ -1,42 +1,42 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const getAuthToken = (user) => {
-    if (!(user?.['_id'] && user?.role)) {
-        throw new Error("Invalid user data");
-    }
+  if (!(user?.['_id'] && user?.role)) {
+    throw new Error('Invalid user data')
+  }
 
-    return jwt.sign(
-        {
-            id: user._id,
-            roles: user.roles,
-            iat: Math.floor(Date.now() / 1000), 
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '1h',
-        }
-    );
-};
+  return jwt.sign(
+    {
+      id: user._id,
+      roles: user.roles,
+      iat: Math.floor(Date.now() / 1000),
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '1h',
+    }
+  )
+}
 
 const getRefreshToken = (user) => {
-    return jwt.sign(
-        {
-            id: user._id,
-            roles: user.roles,
-            iat: Math.floor(Date.now() / 1000),
-        },
-        process.env.JWT_REFRESH_SECRET,
-        {
-            expiresIn: '5d',
-        }
-    );
-};
+  return jwt.sign(
+    {
+      id: user._id,
+      roles: user.roles,
+      iat: Math.floor(Date.now() / 1000),
+    },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: '5d',
+    }
+  )
+}
 
 const generateToken = (user) => {
-    const authToken = getAuthToken(user);
-    const refreshToken = getRefreshToken(user);
-    return { authToken, refreshToken };
-};
+  const authToken = getAuthToken(user)
+  const refreshToken = getRefreshToken(user)
+  return { authToken, refreshToken }
+}
 
-module.exports = generateToken;
+module.exports = generateToken
