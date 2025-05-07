6ccd58c6ffb9056c4f61c5c732e17c17b4a0979b
Nweke Chinonso Christian <nwekechristian890@gmail.com>
2025-05-07 07:11:45 -0700

    made changes in proofile view

--- api/controller/profile_view.js
+++ api/controller/profile_view.js
@@ -1,6 +1,14 @@
 const Profile = require('../model/Profile');
 const User = require('../model/User');
 
+exports.getAllUserProfile = async (req, res) => {
+    try {
+        const profiles = await Profile.find();
+        res.status(200).json(profiles);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
 exports.getUserProfile = async (req, res) => {
     try {
         const { id } = req.params.user_id;
@@ -24,7 +32,7 @@ exports.postProfileImage = async (req, res) => {
         if (!profile) {
             return res.status(404).json({ message: 'Profile not found' });
         }
-        profile.img = req.file.path; 
+        profile.img = req.file.path;
         await profile.save();
         res.status(200).json({
             message: 'Profile image uploaded successfully',
--- api/route/profileRoute.js
+++ api/route/profileRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const profileController = require('../controller/profile_view');
 const router = express.Router();
 
+router.get('/all', profileController.getAllUserProfile)
 router.get('/:id/get', profileController.getUserProfile);
 router.post('/:id/upload', profileController.postProfileImage);
 router.put('/:id/update', profileController.updateUserProfile);
