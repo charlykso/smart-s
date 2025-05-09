75074d3f1159817e6bffdd29f1d26e281091c12a
obilalumark <lumarkobila@icloud.com>
2025-05-09 14:23:50 +0100

    changes made

--- api/controller/Session_view.js
+++ api/controller/Session_view.js
@@ -1,5 +1,6 @@
 const Session = require('../model/Session'); 
 const School = require('../model/School');
+const { response } = require('express');
 
 
 exports.getAllSessions = async (req, res) => {
@@ -28,6 +29,8 @@ exports.createSession = async (req, res) => {
         const {school: school_id, name, startDate, endDate} = req.body;
         const school = await School.findById(school_id)
         if(!school) return res.status(404).json({message: 'School not found'});
+        const existingSession = await Session.findOne({name})
+        if(existingSession) {return res.status(400).json({message: "session already exists"})};
         const session = new Session({school: school_id, name, startDate, endDate });
         await session.save();
         res.status(201).json(session);
