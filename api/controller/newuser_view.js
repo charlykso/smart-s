5b2f744b7e2e32505c37e54f59bd31f7dc92e81b
obilalumark <lumarkobila@icloud.com>
2025-05-09 11:52:57 +0100

    newuser_view.js

---
+++ api/controller/newuser_view.js
@@ -0,0 +1,19 @@
+const User = require("../model/User");
+const Profile = require("../model/Profile");
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+            .select('-password -__v')
+            .populate('school', 'name')
+            .populate('classArm', 'name')
+            .populate({
+                path: Profile, 
+                select: 'bio location' 
+            });
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
