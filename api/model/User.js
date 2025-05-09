6cf2e3eb60c84b38ce6426ea6ffa750b0f89afbb
Nweke Chinonso Christian <nwekechristian890@gmail.com>
2025-05-09 03:56:17 -0700

    removed user in profile model

--- api/model/User.js
+++ api/model/User.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+    profile: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Profile',
+    },
     school: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'School',
