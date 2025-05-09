5b1d7e53f2b66bc907def3718ffc35f46457dc4a
Charles Madu <charlesmadu001@gmail.com>
2025-05-09 15:16:45 +0100

    changes made

--- api/controller/Fee_view.js
+++ api/controller/Fee_view.js
@@ -33,7 +33,7 @@ exports.createFee = async (req, res) => {
         isApproved: req.body.isApproved,
     });
     try {
-        const existing = await Fee.findOne({ $or: [{ name: req.body.name }, { term: req.body.term }] })
+        const existing = await Fee.findOne({ $or: [{ name: req.body.name }] })
         if (existing) return res.status(409).json({ message: 'This Fee already exist' });
         const term = await Term.findById(term_id)
         if (!term) return res.status(409).json({message: "Term not found"})
