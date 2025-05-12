7c9120a6483899b8006a7b1f82ced8f0361e92a5
Charles Madu <charlesmadu001@gmail.com>
2025-05-12 14:49:34 +0100

    protected route

--- api/route/approveRoute.js
+++ api/route/approveRoute.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const FeeController = require('../controller/approve_view')
+const authenticateToken = require('../middleware/authenticateToken')
+const roleList = require('../helpers/roleList')
+const verifyRoles = require('../middleware/verifyRoles')
 const router = express.Router()
 
-router.put('/:fee_id/approve', FeeController.approveFee)
+router.route('/:fee_id/approve')
+    .put(authenticateToken, verifyRoles(roleList.principal), FeeController.approveFee) //Principal
 
 module.exports = router
--- api/route/schoolRoute.js
+++ api/route/schoolRoute.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const SchoolController = require('../controller/school_view')
+const authenticateToken = require('../middleware/authenticateToken')
+const roleList = require('../helpers/roleList')
+const verifyRoles = require('../middleware/verifyRoles')
 const router = express.Router()
 
 router.get('/all',  SchoolController.getSchools)
-router.get('/:id', SchoolController.getSchool)
-router.post('/create', SchoolController.createSchool)
-router.put('/:id/update', SchoolController.updateSchool)
-router.delete('/:id/delete', SchoolController.deleteSchool)
-router.get('/by-address/:address_id', SchoolController.getSchoolByAddress)
-router.get('/by-groupSchool/:groupSchool_id', SchoolController.getSchoolByGroupSchool)
+router.route('/:id')
+ .get(authenticateToken, verifyRoles(roleList.principal), SchoolController.getSchool) //principal
+router.route('/create')
+ .post(authenticateToken, verifyRoles(roleList.admin), SchoolController.createSchool) //Admin
+
+router.route('/:id/update')
+.put(authenticateToken, verifyRoles(roleList.admin), SchoolController.updateSchool) //Admin
+router.route('/:id/delete')
+ .delete(authenticateToken, verifyRoles(roleList.admin), SchoolController.deleteSchool) //Admin
+router.route('/by-address/:address_id')
+ .get(authenticateToken, verifyRoles(roleList.principal), SchoolController.getSchoolByAddress) //principal
+
+router.route('/by-groupSchool/:groupSchool_id')
+.get(authenticateToken, verifyRoles(roleList.principal), SchoolController.getSchoolByGroupSchool) //principal
 
 module.exports = router
