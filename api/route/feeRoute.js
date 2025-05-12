dddba0e8adc341b4421e84bc9bf77896a30b19be
Charles Madu <charlesmadu001@gmail.com>
2025-05-12 14:50:59 +0100

    changes made

--- api/route/feeRoute.js
+++ api/route/feeRoute.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/all', FeeController.getFees)
 router.get('/:id', FeeController.getFee)
 router.route('/create')
- .post(authenticateToken, verifyRoles(roleList.bursar, ), FeeController.createFee) //Bursar
+ .post(authenticateToken, verifyRoles(roleList.bursar), FeeController.createFee) //Bursar
 router.route('/:id/update')
  .put(authenticateToken, verifyRoles(roleList.bursar), FeeController.updateFee) //Bursar
 router.route('/:id/delete')
