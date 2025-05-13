2a7817205c4a948fd8c548b46ee47b3b502caa55
Charles Madu <charlesmadu001@gmail.com>
2025-05-13 21:33:31 +0100

    changes made in all the route

--- api/route/approveRoute.js
+++ api/route/approveRoute.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const FeeController = require('../controller/approve_view')
+const ApproveController = require('../controller/approve_view')
 const authenticateToken = require('../middleware/authenticateToken')
 const roleList = require('../helpers/roleList')
 const verifyRoles = require('../middleware/verifyRoles')
 const router = express.Router()
 
 router.route('/:fee_id/approve')
-    .put(authenticateToken, verifyRoles(roleList.principal), FeeController.approveFee) //Principal
+    .put(authenticateToken, verifyRoles(roleList.principal), ApproveController.approveFee) //Principal
 
 module.exports = router
