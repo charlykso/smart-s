a67d0c5d95396bbb82845f3b9b4d86e8e3a020d4
Charles Madu <charlesmadu001@gmail.com>
2025-05-14 14:14:39 +0100

    changes made

--- api/route/approveRoute.js
+++ api/route/approveRoute.js
@@ -6,6 +6,6 @@ const verifyRoles = require('../middleware/verifyRoles')
 const router = express.Router()
 
 router.route('/:fee_id/approve')
-    .put(authenticateToken, verifyRoles(roleList.principal), approveController.approveFee) //Principal
+    .put(authenticateToken, verifyRoles(roleList.principal, roleList.admin), approveController.approveFee) //Principal
 
 module.exports = router
